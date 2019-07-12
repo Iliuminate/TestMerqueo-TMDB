43de90a05f25b88de3a0c5a79a4a17bc9b0fa434
Carlos Diaz <carlos.diaz@grupocmc.co>
2019-07-12 08:57:25 -0500

    Creación del proyecto

---
+++ TestMerqueoTMDB/TestMerqueoTMDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestMerqueoTMDB
+//
+//  Created by Usuario on 7/12/19.
+//  Copyright © 2019 Ilmint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
