3e50cf80c94b7c148bb89ed3527d6c034c26b33b
Carlos Diaz <carlos.diaz@grupocmc.co>
2019-07-12 11:18:22 -0500

    prueba del gitignore

---
+++ TestMerqueoTMDB/TestMerqueoTMDB/PopularMovies/PopularMoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PopularMoviesViewController.swift
+//  TestMerqueoTMDB
+//
+//  Created by Usuario on 7/12/19.
+//  Copyright © 2019 Ilmint. All rights reserved.
+//
+
+import UIKit
+
+class PopularMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
